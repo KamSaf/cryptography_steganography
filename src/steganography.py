9f9ed02ae81bf75edf3ce5165e25a7e7f70fd356
KamSaf <bopaf42@gmail.com>
2024-04-06 23:07:20 +0200

    Created steganography decoding function

--- src/steganography.py
+++ src/steganography.py
@@ -14,6 +14,20 @@ def bits_provider(message) -> Iterable[int]:
                 yield int(bit)
 
 
+def chars_provider(pixel_red_values) -> Iterable[str]:
+    """
+    """
+    b_str = ''
+    for i, pixel_red_value in enumerate(pixel_red_values):
+        if i != 0 and i % 8 == 0:
+            if int(b_str, 2) == 0:
+                break
+            char = chr(int(b_str, 2))
+            b_str = ''
+            yield char
+        b_str += str(format(pixel_red_value, 'b'))[-1]
+
+
 def clear_low_order_bits(pixels) -> None:
     """
     """
@@ -32,28 +46,13 @@ def create_image(message, input_file, output_file) -> None:
     if len(message) * 8 > pixels.shape[1]:
         raise Exception('Message is too long to save in given file')
     for i, bit in enumerate(bits_provider(message)):
+        pass
         row = i // pixels.shape[1]
         col = i % pixels.shape[1]
         pixels[row, col][0] |= bit  # |= means sum of the bits first and second value
     img = Image.fromarray(pixels)
     img.save(output_file)
     img.close()
-
-
-def chars_provider(pixel_red_values) -> Iterable[str]:
-    ascii_value = 0
-    for i, pixel_red_value in enumerate(pixel_red_values):
-        ascii_value_bit_position = 7 - i % 8
-        if pixel_red_value & 1:
-            ascii_value |= 1 << ascii_value_bit_position
-        if ascii_value_bit_position == 0:
-            char: str = chr(ascii_value)
-            if not char.isprintable() and char != '\n':
-                return
-
-            yield char
-
-            ascii_value = 0
 
 
 def decode_image(input_file: str) -> str:
@@ -63,12 +62,3 @@ def decode_image(input_file: str) -> str:
     result = ''.join(chars_provider(img.getdata(band=0)))  # band=0 means only red color is returned from getdata()
     img.close()
     return result
-
-
-# message = """Unless you have a strong reason and know what you're doing, you should use bitwise operators only for controlling bits. Its too easy to get it wrong otherwise. In most cases, youll want to pass integers as arguments to the bitwise operators."""
-
-message = 'there'
-
-create_image(message=message, input_file='file.png', output_file='file.png')
-
-print(decode_image('file.png'))
